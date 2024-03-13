3bafb249059c4397e817a37be6d8f97a1f6bb956
Nuengmon <0523nada@gmail.com>
2024-03-13 19:12:24 +0900

    페이지 리뉴얼

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-// let Home = document.querySelector('#Home');
-// let Skill = document.querySelector('#Skill');
-// let Project = document.querySelector('#Project');
-// let Contact = document.querySelector('#Contact');
-
 let Home_link = document.querySelector('#Home_link');
 let Skill_link = document.querySelector('#Skill_link');
 let Project_link = document.querySelector('#Project_link');
@@ -10,6 +5,18 @@ let Contact_link = document.querySelector('#Contact_link');
 let Main = document.querySelector('.main');
 let Sub_list = document.querySelectorAll('.Sub_list');
 let Move_list = document.querySelectorAll('.move_point');
+let Comment = document.querySelectorAll('.comment');
+let move_point_list = document.querySelectorAll('.move_point');
+
+let wrap = document.getElementsByClassName('main')[0]; // 보일 영역
+let content = document.getElementsByClassName('content');
+let page = 0; // 영역 포지션 초기값
+let lastPage = content.length - 1;
+
+let short = document.getElementsByClassName('short').length;
+
+history.scrollRestoration = "manual"
+
 
 // nav를 누르면 하위 레이아웃을 활성화 시켜주는 함수
 function Switch_toggle(ID) {
@@ -21,7 +28,7 @@ function Switch_toggle(ID) {
 
     for (i = 0; i < Sub_list.length; i++) {
         Sub_arr.push(Sub_list.item(i));
-        
+
     }
     for (i = 0; i < Move_list.length; i++) {
         Move_arr.push(Move_list.item(i));
@@ -35,7 +42,8 @@ function Switch_toggle(ID) {
             i--;
         }
     }
-    
+
+
     for (i = 0; i < Move_arr.length; i++) {
         Move_arr[i].classList.remove('invisible');
         Move_arr.splice(i, 1);
@@ -52,6 +60,8 @@ function Switch_toggle(ID) {
         Move_arr[i].classList.add('invisible');
 
     }
+
+
 
 };
 
@@ -86,20 +96,68 @@ Hamburger.addEventListener('click', function () {
 
     }
 
-    if (Hamburger.classList.item('active')) {
+    if (Hamburger_wrap.classList.item(1) == 'active') {
         Nav_arr[0].classList.add('active');
         Move_arr[0].classList.remove('invisible');
         Sub_arr[0].classList.remove('invisible');
+    }
+    else {
+        Nav_arr[0].classList.remove('active');
+        Move_arr[0].classList.add('invisible');
+        Sub_arr[0].classList.add('invisible');
     }
 
     Main.classList.toggle('active');
 
 
-
-
-
-
-    
-
 });
 
+
+
+
+
+window.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    console.log(e.deltaY)
+
+    if (e.deltaY > 0) {
+        page++;
+    } else if (e.deltaY < 0) {
+        page--;
+    }
+    if (page < 0) {
+        page = 0;
+    } else if (page > lastPage) {
+        page = lastPage;
+    }
+
+    if (page < lastPage - 1) {
+        wrap.style.top = page * -100 + 'vh';
+    }
+    else {
+        // wrap.style.top = (lastPage - short) * -100 + (page - short) * - 20 + 'vh';
+        wrap.style.top = (lastPage - short) * - 100 + - 30 + 'vh';
+
+    }
+
+
+    console.log(page);
+}, { passive: false }); // 디폴트 기능 제거 - 스크롤
+
+
+for (let i = 0; i < move_point_list.length; i++) {
+    move_point_list[i].addEventListener('click', function () {
+        page = i;
+
+        if (page < lastPage - 1) {
+            wrap.style.top = page * -100 + 'vh';
+        }
+        else {
+            // wrap.style.top = (lastPage - short) * -100 + (page - short) * - 20 + 'vh';
+            wrap.style.top = (lastPage - short) * - 100 + - 30 + 'vh';
+    
+        }
+        // wrap.style.top = page * -100 + 'vh';
+        // console.log(page);
+    });
+}
